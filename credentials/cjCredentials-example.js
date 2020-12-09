e4d7bd209fdc6ad375da4c1964ab0275c2e7eb8a
Pakpoom Tiwakornkit <apt.enjoy@gmail.com>
2020-12-09 21:17:08 +0700

    provide credential example files

---
+++ credentials/cjCredentials-example.js
@@ -0,0 +1,5 @@
+module.exports = {
+	apiKey: process.env.CJ_API_KEY || 'xxxxxxxx',
+	connectionListURL: "https://developers.cjdropshipping.com/api/product/connectList",
+	productDetailsURL: "https://developers.cjdropshipping.com/api/product/queryProducts"
+}
---
+++ credentials/shopeeCredentials-example.js
@@ -0,0 +1,6 @@
+module.exports = {
+	partnerId: process.env.SHOPEE_PARTNER_ID || "xxxxx",
+	partnerKey: process.env.SHOPEE_PARTNER_KEY || "xxxxxxx",
+	testCallbackURL: "http://example.com",
+	productionCallbackURL: "http://example.com"
+}
