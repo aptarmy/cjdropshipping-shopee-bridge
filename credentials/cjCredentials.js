c216cbc7c48e543bad3d9a61813f900442e70466
Pakpoom Tiwakornkit <apt.enjoy@gmail.com>
2020-12-10 17:22:02 +0700

    fix: show credential files as sensitive data will be stored in environment variable

--- credentials/cjCredentials.js
+++ credentials/cjCredentials.js
@@ -1,5 +1,5 @@
 module.exports = {
-	apiKey: process.env.CJ_API_KEY || 'xxxxxxxx',
+	apiKey: process.env.CJ_API_KEY,
 	connectionListURL: "https://developers.cjdropshipping.com/api/product/connectList",
 	productDetailsURL: "https://developers.cjdropshipping.com/api/product/queryProducts"
 }
--- credentials/shopeeCredentials.js
+++ credentials/shopeeCredentials.js
@@ -1,6 +1,7 @@
 module.exports = {
-	partnerId: process.env.SHOPEE_PARTNER_ID || "xxxxx",
-	partnerKey: process.env.SHOPEE_PARTNER_KEY || "xxxxxxx",
+	shopId: parseInt(process.env.SHOPEE_SHOP_ID),
+	partnerId: parseInt(process.env.SHOPEE_PARTNER_ID),
+	partnerKey: process.env.SHOPEE_PARTNER_KEY,
 	testCallbackURL: process.env.SHOPEE_TEST_CALLBACK_URL || "http://example.com",
 	productionCallbackURL: process.env.SHOPEE_PROD_CALLBACK_URL || "http://example.com"
 }
